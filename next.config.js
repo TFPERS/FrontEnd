495251a3965dc38634e496d3a08213c81f876372
kenkenm43 <kenkenm43@gmail.com>
2022-05-10 11:35:16 +0700

    UPDATE

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  assetPrefix: isProd ? "/TFPERS/FrontEnd" : "",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
