00131fc7e90f2f84fe19c92694922b4d3eb930d3
kenkenm43 <kenkenm43@gmail.com>
2022-05-23 12:25:57 +0700

    UPDATE follow

--- src/pages/petition/follow/index.tsx
+++ src/pages/petition/follow/index.tsx
@@ -3,13 +3,74 @@ import Link from "next/link";
 import Layout from "../../../components/Layout/index";
 import { useState, useEffect } from "react";
 import axios from "../../../config/axios.config";
+import AuthService from "../../../services/auth.service";
+import dayjs from "dayjs";
 
 function Follow() {
+  const [petitions, setPetitions] = useState<any>([]);
+  useEffect(() => {
+    const fetchPetition = async () => {
+      const { data } = await axios.get(
+        `/api/petition/me/${AuthService.getCurrentUser().id}`
+      );
+      setPetitions(data);
+    };
+    fetchPetition();
+  }, []);
+
+  const formatTypePetition = (type: any) => {
+    if (type === "extendPayment") {
+      return "ขยายเวลาชำระเงิน";
+    }
+  };
+
+  const formatStatus = (status: any) => {
+    if (status === "pending") {
+      return (
+        <div className="bg-[#FFC72C] text-primary-white w-[11.25rem] h-[3.75rem] rounded-[0.625rem] flex items-center justify-center">
+          กำลังดำเนินการ
+        </div>
+      );
+    }
+    if (status === "success") {
+      <div className="bg-[#17A87B] text-primary-white w-[11.25rem] h-[3.75rem] rounded-[0.625rem] flex items-center justify-center">
+        เสร็จสิ้น
+      </div>;
+    }
+  };
+  const formatDD = (date: any) => {
+    const format = dayjs(date).format("DD/MM/YYYY \n HH:mm A");
+    return <div>{format}</div>;
+  };
+
   return (
     <Layout>
-      <div className="bg-primary-white p-8 rounded-[0.625rem] w-[1200px] mx-auto">
-        <div className="text-4xl font-semibold">ติดตามคำร้องและสถานะ</div>
-        <div></div>
+      <div className="bg-primary-white rounded-[0.625rem] w-[1200px] h-[700px] mx-auto mt-10">
+        <div className="text-4xl font-semibold p-8 ">ติดตามคำร้องและสถานะ</div>
+        <table className="table-fixed w-full text-center text-2xl">
+          <thead className="border-y-4">
+            <tr>
+              <th className="p-3">หมายเลขคำร้อง</th>
+              <th>ประเภทคำร้อง</th>
+              <th>สถานะคำร้อง</th>
+              <th>วันที่</th>
+            </tr>
+          </thead>
+          <tbody>
+            {petitions.map((petition: any) => (
+              <tr key={petition.id}>
+                <td className="py-7">{petition.id}</td>
+                <td>{formatTypePetition(petition.type)}ค่าลงทะเบียน</td>
+                <td>
+                  <div className="flex justify-center">
+                    {formatStatus(petition.status)}
+                  </div>
+                </td>
+                <td>{formatDD(petition.createdAt)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
       <Link href="/petition">back </Link>
     </Layout>
