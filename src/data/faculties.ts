bd2c4609998432110d4197abeadddd3c15dcf7ba
Khomsan Chuechuphaeng <soua214@gmail.com>
2022-08-22 21:52:41 +0700

    [Add]Data Bachelor’s Degree

--- src/data/faculties.ts
+++ src/data/faculties.ts
@@ -1,1 +1,7 @@
-export const faculties = [{ value: "SIT", label: "เทคโนโลยีสารสนเทศ" }];
+export const faculties = [{ value: "SIT", label: "เทคโนโลยีสารสนเทศ" },
+{ value: "FOE", label: "วิศวกรรมศาสตร์" },
+{ value: "FSCI", label: "วิทยาศาสตร์" },
+{ value: "FIET", label: "ครุศาสตร์อุตสาหกรรมและเทคโนโลยี" },
+{ value: "SOAD", label: "สถาปัตยกรรมศาสตร์และการออกแบบ" },
+{ value: "FIBO", label: "สถาบันวิทยาการหุ่นยนต์ภาคสนาม" },
+];
--- src/data/majors.ts
+++ src/data/majors.ts
@@ -16,4 +16,217 @@ export const majors = [
       },
     ],
   },
+  {
+    name: "FOE",
+    majors: [
+      {
+        value: "Chemical",
+        label: "วิศวกรรมเคมี ",
+      },
+      {
+        value: "Chemical(International Program)",
+        label: "วิศวกรรมเคมี (หลักสูตรนานาชาติ) ",
+      },
+      {
+        value: "Mechanical",
+        label: "วิศวกรรมเครื่องกล",
+      },
+      {
+        value: "Automotive",
+        label: "วิศวกรรมยานยนต์  ",
+      },
+      {
+        value: "Electrical",
+        label: "วิศวกรรมไฟฟ้า",
+      },
+      {
+        value: "Electrical Engineering (Power System, Power Electronics and Energy)",
+        label: "วิศวกรรมไฟฟ้า (ระบบไฟฟ้า อิเล็กทรอนิกส์ และพลังงาน)",
+      },
+      {
+        value: "Civil",
+        label: "วิศวกรรมโยธา",
+      },
+      {
+        value: "Civil Engineering (International Program)",
+        label: "วิศวกรรมโยธา (หลักสูตรนานาชาติ) ",
+      },
+      {
+        value: "Production",
+        label: "วิศวกรรมอุตสาหการ",
+      },
+      {
+        value: "Computer",
+        label: "วิศวกรรมคอมพิวเตอร์ ",
+      },
+      {
+        value: "Computer Engineering (International Program) ",
+        label: "วิศวกรรมคอมพิวเตอร์ (หลักสูตรนานาชาติ)",
+      },
+      {
+        value: "Health Data Science",
+        label: "วิทยาศาสตร์ข้อมูลสุขภาพ",
+      },
+      {
+        value: "Control Systems and Instrumentation",
+        label: "วิศวกรรมระบบควบคุมและเครื่องมือวัด",
+      },
+      {
+        value: "Environmental",
+        label: "วิศวกรรมสิ่งแวดล้อม",
+      },
+      {
+        value: "Environmental Engineering(International Program)",
+        label: "วิศวกรรมสิ่งแวดล้อม (หลักสูตรนานาชาติ) ",
+      },
+      {
+        value: "Electrical Communication and Electronic",
+        label: "วิศวกรรมไฟฟ้าสื่อสารและอิเล็กทรอนิกส์",
+      },
+      {
+        value: "Electrical Communication and Electronic Engineering (International Program)",
+        label: "วิศวกรรมอิเล็กทรอนิกส์และสารสนเทศสื่อสาร (หลักสูตรนานาชาติ) ",
+      },
+      {
+        value: " Materials",
+        label: "วิศวกรรมวัสดุ",
+      },
+      {
+        value: "Tool",
+        label: "วิศวกรรมเครื่องมือ",
+      },
+      {
+        value: "Automation ",
+        label: "วิศวกรรมอัตโนมัติ (หลักสูตรนานาชาติ)",
+      },
+      {
+        value: "Mechatronics ",
+        label: "วิศวกรรมเมคคาทรอนิกส์",
+      },
+      {
+        value: "Modern Automotive and Aerospace Parts Manufacturing",
+        label: "วิศวกรรมการผลิตชิ้นส่วนยานยนต์และอากาศยานสมัยใหม่",
+      },
+    ],
+  },
+  {
+    name: "FSCI",
+    majors: [
+      {
+        value: "Mathematics",
+        label: "คณิตศาสตร์",
+      },
+      {
+        value: "Chemistry",
+        label: "เคมี",
+      },
+      {
+        value: "Applied Physics",
+        label: "ฟิสิกส์",
+      },
+      {
+        value: "Microbiology",
+        label: "จุลชีววิทยา",
+      },
+      {
+        value: "Applied Computer Science",
+        label: "วิทยาการคอมพิวเตอร์ประยุกต์",
+      },
+      {
+        value: "Statistics",
+        label: "สถิติและวิทยาการข้อมูล",
+      },
+      {
+        value: "Food Science and Technology",
+        label: "วิทยาศาสตร์และเทคโนโลยีการอาหาร",
+      },
+    ],
+  },
+  {
+    name: "FIET",
+    majors: [
+      {
+        value: "Packaging and Printing",
+        label: "เทคโนโลยีบรรจุภัณฑ์และการพิมพ์ ",
+      },
+      {
+        value: "Industrial",
+        label: "เทคโนโลยีอุตสาหกรรม",
+      },
+      {
+        value: "Applied Computer Science-Multimedia",
+        label: "วิทยาการคอมพิวเตอร์ประยุกต์-มัลติมีเดีย ",
+      },
+      {
+        value: "Mechanical Engineering (5 Years Program)",
+        label: "วิศวกรรมเครื่องกล (หลักสูตร 5 ปี) ",
+      },
+      {
+        value: "Electrical Engineering (5 Years Program)",
+        label: "วิศวกรรมไฟฟ้า (หลักสูตร 5 ปี)",
+      },
+      {
+        value: "Civil Engineering (5 Years Program)",
+        label: "วิศวกรรมโยธา (หลักสูตร 5 ปี)",
+      },
+      {
+        value: "Production Engineering (5 Years Program)",
+        label: "วิศวกรรมอุตสาหการ (หลักสูตร 5 ปี)",
+      },
+      {
+        value: "Educational Technology and Mass Communication",
+        label: "เทคโนโลยีการศึกษาและสื่อสารมวลชน",
+      },
+    ],
+  },
+  {
+    name: "SOAD",
+    majors: [
+      {
+        value: "Architecture (International Program)",
+        label: "สถาปัตยกรรม (หลักสูตรนานาชาติ)",
+      },
+      {
+        value: "Interior Architecture (International Program)",
+        label: "สถาปัตยกรรมภายใน (หลักสูตรนานาชาติ) ",
+      },
+      {
+        value: "Design Innovation (International Program)",
+        label: "นวัตกรรมการออกแบบ (หลักสูตรนานาชาติ)",
+      },
+      {
+        value: "Communication Design (International Program)",
+        label: "ออกแบบนิเทศศิลป์ (หลักสูตรนานาชาติ)",
+      },
+      {
+        value: "Landscape Architecture (International Program)",
+        label: "ภูมิสถาปัตยกรรม (หลักสูตรนานาชาติ)",
+      },
+      {
+        value: "Multiple Intelligence for Design Integration (International Program)",
+        label: "การบูรณาการการออกแบบด้วยพหุปัญญา (หลักสูตรพหุวิทยาการ, หลักสูตรนานาชาติ)",
+      },
+      {
+        value: "Media Technology",
+        label: "เทคโนโลยีมีเดีย",
+      },
+      {
+        value: "Medical and science",
+        label: "มีเดียทางการแพทย์และวิทยาศาสตร์",
+      },
+      {
+        value: "Media Arts",
+        label: " มีเดียอาตส์ ",
+      },
+    ],
+  },
+  {
+    name: "FIBO",
+    majors: [
+      {
+        value: "Robotics and Automation Engineering",
+        label: "วิศวกรรมหุ่นยนต์และระบบอัตโนมัติ",
+      }
+    ],
+  },
 ];
