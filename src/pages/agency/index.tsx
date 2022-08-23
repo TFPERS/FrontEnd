3f1611d98b38023ad411b8fb295c11896cb7f7d4
Khomsan Chuechuphaeng <soua214@gmail.com>
2022-08-23 21:17:22 +0700

    [Edit] Index_Agency

--- src/pages/agency/index.tsx
+++ src/pages/agency/index.tsx
@@ -5,28 +5,28 @@ import Image from "next/image";
 
 const agency = () => {
   return (
-    <div className="min-h-screen flex">
+    <div className="flex min-h-screen">
       <div className="bg-slate-700 min-w-[270px]">
         <Sidebar />
       </div>
-      <div className="bg-white w-full">col2</div>
+      <div className="w-full bg-white">col2</div>
       <div className="bg-white min-w-[270px] py-8 pr-7 flex flex-col space-y-5 ">
         <Card>
-          <div className="flex flex-col justify-center items-center h-full space-y-5">
+          <div className="flex flex-col items-center justify-center h-full space-y-5">
             <Image
               src="/images/Mask_user_profile.png "
               alt="HappyStudent"
               width={150}
               height={150}
             />
-            <div className="">กลุ่มงานช่วยเหลือทางการ เงินนักศึกษา</div>
+            <div className="text-xl">กลุ่มงานช่วยเหลือทางการเงินนักศึกษา</div>
           </div>
         </Card>
         <Card>
-          <div>การแจ้งเตือน</div>
-          <div className="overflow-auto h-5/6 space-y-4 " id="journal-scroll">
+          <div className="text-xl">การแจ้งเตือน</div>
+          <div className="space-y-4 overflow-auto h-5/6 " id="journal-scroll">
             <div className="rounded-[0.625rem] text-black flex h-24 w-full bg-white">
-              <div className="w-1/6 self-center text-black bg-black m-2">
+              <div className="self-center w-1/6 m-2 text-black bg-black">
                 <Image
                   src="/images/Mask_user_profile.png "
                   alt="HappyStudent"
@@ -34,37 +34,13 @@ const agency = () => {
                   height={30}
                 />
               </div>
-              <div className="flex flex-col overflow-auto w-5/6 text-left">
-                <div>กลุ่มงานช่วยเหลือทางการเงินแก่นักศึกษา</div>
-                <div>มีการอัพเดตนโยบายจากทางมหาวิทยาลัยภายในปี 20222</div>
-              </div>
-            </div>
-            <div className="rounded-[0.625rem] text-black flex h-24 w-full bg-white">
-              <div className="w-1/6 self-center text-black bg-black m-2">
-                <Image
-                  src="/images/Mask_user_profile.png "
-                  alt="HappyStudent"
-                  width={30}
-                  height={30}
-                />
-              </div>
-              <div className="flex flex-col overflow-auto w-5/6 text-left">
-                <div>กลุ่มงานช่วยเหลือทางการเงินแก่นักศึกษา</div>
-                <div>มีการอัพเดตนโยบายจากทางมหาวิทยาลัยภายในปี 20222</div>
-              </div>
-            </div>
-            <div className="rounded-[0.625rem] text-black flex h-24 w-full bg-white">
-              <div className="w-1/6 self-center text-black bg-black m-2">
-                <Image
-                  src="/images/Mask_user_profile.png "
-                  alt="HappyStudent"
-                  width={30}
-                  height={30}
-                />
-              </div>
-              <div className="flex flex-col overflow-auto w-5/6 text-left">
-                <div>กลุ่มงานช่วยเหลือทางการเงินแก่นักศึกษา</div>
-                <div>มีการอัพเดตนโยบายจากทางมหาวิทยาลัยภายในปี 20222</div>
+              <div className="flex flex-col w-5/6 overflow-auto text-left">
+                <div className="font-semibold">
+                  กลุ่มงานช่วยเหลือทางการเงินแก่นักศึกษา
+                </div>
+                <div className="text-sm ">
+                  มีการอัพเดตนโยบายจากทางมหาวิทยาลัยภายในปี 20222
+                </div>
               </div>
             </div>
           </div>
