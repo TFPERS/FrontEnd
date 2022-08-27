a04f5e7f18c76e13c4d03ff0e696d55cafaa3168
kenkenm43 <kenkenm43@gmail.com>
2022-08-27 19:52:33 +0700

    UPDATE notification

--- src/pages/agency/notification.tsx
+++ src/pages/agency/notification.tsx
@@ -4,7 +4,9 @@ import Agency from "../../components/Layout/Agency";
 const notice = () => {
   return (
     <Agency>
-      <div>notice</div>
+      <div className="flex flex-col space-y-5 h-full">
+        <div className="text-3xl">แจ้งเตือน</div>
+      </div>
     </Agency>
   );
 };
