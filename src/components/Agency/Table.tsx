dc7b0efd452221c6321b5091992a165c86d63645
kenkenm43 <kenkenm43@gmail.com>
2022-08-30 13:55:02 +0700

    UPDATE  application

--- src/components/Agency/Table.tsx
+++ src/components/Agency/Table.tsx
@@ -1,27 +1,59 @@
-import React from "react";
-
-const Table = () => {
+import React, { useState } from "react";
+import dayjs from "dayjs";
+import Paginate from "../../components/Paginate";
+import axios from "../../config/axios.config";
+type Props = {
+  petitions: any;
+};
+const formatStatus = (status: any) => {
+  if (status === "pending") {
+    return <div>กำลังดำเนินการ</div>;
+  }
+  if (status === "success") {
+    <div>เสร็จสิ้น</div>;
+  }
+};
+const formatDD = (date: any) => {
+  const format = dayjs(date).format("DD/MM/YYYY \n HH:mm A");
+  return <div>{format}</div>;
+};
+const Table = ({ petitions }: Props) => {
   return (
-    <table className={`table-fixed w-full text-center overflow-auto`}>
-      <thead className="border-y-4">
-        <tr>
-          <th>รหัสเอกสาร</th>
-          <th>รหัสนักศึกษา</th>
-          <th>สถานะคำร้อง</th>
-          <th>วันที่</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td className="py-7">4</td>
-          <td className="overflow-auto">ค่าลงทะเบียน</td>
-          <td>
-            <div className="flex justify-center"></div>
-          </td>
-          <td></td>
-        </tr>
-      </tbody>
-    </table>
+    <div className="h-full border-2">
+      <table className={`table-fixed w-full text-center overflow-auto`}>
+        <thead className="bg-primary-light-orange text-white text-2xl">
+          <tr>
+            <th className="py-9">รหัสเอกสาร</th>
+            <th>รหัสนักศึกษา</th>
+            <th>สถานะคำร้อง</th>
+            <th>วันที่</th>
+          </tr>
+        </thead>
+        <tbody>
+          {petitions.map((petition: any) => (
+            <tr key={petition.id}>
+              <td className="py-7">{petition.id}</td>
+              <td className="overflow-auto">
+                {petition.student ? petition.student.id : ""}
+              </td>
+              <td>
+                <div className="flex justify-center">
+                  {formatStatus(petition.status)}
+                </div>
+              </td>
+              <td>{formatDD(petition.createdAt)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {petitions.length === 0 ? (
+        <div className="text-center h-4/6 my-auto text-3xl py-60">
+          ไม่มีคำร้อง
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
   );
 };
 
--- src/pages/agency/application.tsx
+++ src/pages/agency/application.tsx
@@ -1,10 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Agency from "../../components/Layout/Agency";
+import Table from "../../components/Agency/Table";
+import axios from "../../config/axios.config";
+import Paginate from "../../components/Paginate";
 
 const application = () => {
+  const [petitions, setPetitions] = useState<any>([]);
+  const [totalPage, setTotalPage] = useState(0);
+  const [currentPage, setcurrentPage] = useState(1);
+  const handleChange = async (
+    event: React.ChangeEvent<unknown>,
+    value: number
+  ) => {
+    if (value !== currentPage) {
+      setcurrentPage(value);
+      const { data } = await axios.get(
+        `/api/petition/paginate?page=${value - 1}&size=7`
+      );
+      setPetitions(data.content);
+    }
+  };
+
+  useEffect(() => {
+    const fetchPetitions = async () => {
+      const { data } = await axios.get(`/api/petition/paginate?page=0&size=8`);
+      setPetitions(data.content);
+      setTotalPage(data.totalPages);
+    };
+    fetchPetitions();
+  }, []);
   return (
     <Agency>
-      <div>application</div>
+      <div className="flex flex-col h-full space-y-5">
+        <div className="text-3xl">คำร้องขอ</div>
+        <Table petitions={petitions} />
+        {totalPage === 0 ? (
+          ""
+        ) : (
+          <div className="mt-3 max-w-lg bg-white shadow-4xl mx-auto p-2 rounded-2xl">
+            <Paginate
+              totalPage={totalPage}
+              currentPage={currentPage}
+              handleChange={handleChange}
+            />
+          </div>
+        )}
+      </div>
     </Agency>
   );
 };
