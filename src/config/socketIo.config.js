6d895bc3db643f6323972cc2698b1a9f3e467ee1
kenkenm43 <kenkenm43@gmail.com>
2022-09-11 16:09:44 +0700

    UPDATE socketioConfig

--- src/config/socketIo.config.js
+++ src/config/socketIo.config.js
@@ -2,5 +2,5 @@ import { io } from "socket.io-client";
 import getConfig from "next/config";
 const { publicRuntimeConfig } = getConfig();
 const ENDPOINT = publicRuntimeConfig.backendUrl;
-const socket = io(ENDPOINT);
+const socket = io(ENDPOINT, { withCredentials: true });
 export default socket;
