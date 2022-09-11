6bb3522a21c1a8f085363c552c48ab06d79090d6
kenkenm43 <kenkenm43@gmail.com>
2022-09-11 18:49:54 +0700

    UPDATE socketconfig

--- src/config/socketIo.config.js
+++ src/config/socketIo.config.js
@@ -2,5 +2,10 @@ import { io } from "socket.io-client";
 import getConfig from "next/config";
 const { publicRuntimeConfig } = getConfig();
 const ENDPOINT = publicRuntimeConfig.backendUrl;
-const socket = io(ENDPOINT, { withCredentials: true });
+const socket = io(ENDPOINT, {
+  secure: true,
+  rejectUnauthorized: false,
+  path: "/socket.io/",
+  transports: ["websocket"],
+});
 export default socket;
