73b7ad2da92db061169b8dbdfe0d0ef4ab4c9043
kenkenm43 <kenkenm43@gmail.com>
2022-09-11 15:55:54 +0700

    UPDATE socket config

--- src/config/socketIo.config.js
+++ src/config/socketIo.config.js
@@ -2,5 +2,5 @@ import { io } from "socket.io-client";
 import getConfig from "next/config";
 const { publicRuntimeConfig } = getConfig();
 const ENDPOINT = publicRuntimeConfig.backendUrl;
-const socket = io(ENDPOINT, { transports: ["websocket"] });
+const socket = io(ENDPOINT);
 export default socket;
