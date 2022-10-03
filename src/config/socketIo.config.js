aab85929a756d34edc9acd999738690d7d962b1c
kenkenm43 <kenkenm43@gmail.com>
2022-10-03 22:38:09 +0700

    UPDATE updateSocket

--- src/config/socketIo.config.js
+++ src/config/socketIo.config.js
@@ -2,8 +2,7 @@ import { io } from "socket.io-client";
 import getConfig from "next/config";
 const { publicRuntimeConfig } = getConfig();
 const ENDPOINT = publicRuntimeConfig.frontendUrl;
-console.log(ENDPOINT);
-const socket = io(ENDPOINT, {
+const socket = io("https://www.tfpers.software", {
   secure: true,
   rejectUnauthorized: false,
   path: "/socket.io/",
