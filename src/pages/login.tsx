c6a213d05ec9891b2ce9a8c965c6a4d2ed38f302
kenkenm43 <kenkenm43@gmail.com>
2022-10-26 19:41:10 +0700

    UPDATE configENV

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -38,25 +38,27 @@ function Login() {
         router.push("/petition");
       }, 1200);
     } catch (error: any) {
-      // const { data } = error.response;
+      if (error.response) {
+        const { data } = error.response;
+        Swal.fire({
+          title: data.message,
+          // text: "ถ้ายกเลิกคำขอแล้ว คำขอนี้จะไม่สามารถดำเนินการต่อ",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#30d64c",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "ตกลง",
+          cancelButtonText: "ยกเลิก",
+        }).then((result) => {
+          if (result.isConfirmed) {
+            router.push("/register");
+          }
+        });
+        await toast.error(data.message, {
+          theme: "dark",
+        });
+      }
       console.log(error);
-      // Swal.fire({
-      //   title: data.message,
-      //   // text: "ถ้ายกเลิกคำขอแล้ว คำขอนี้จะไม่สามารถดำเนินการต่อ",
-      //   icon: "warning",
-      //   showCancelButton: true,
-      //   confirmButtonColor: "#30d64c",
-      //   cancelButtonColor: "#d33",
-      //   confirmButtonText: "ตกลง",
-      //   cancelButtonText: "ยกเลิก",
-      // }).then((result) => {
-      //   if (result.isConfirmed) {
-      //     router.push("/register");
-      //   }
-      // });
-      // await toast.error(data.message, {
-      //   theme: "dark",
-      // });
     }
   };
 
